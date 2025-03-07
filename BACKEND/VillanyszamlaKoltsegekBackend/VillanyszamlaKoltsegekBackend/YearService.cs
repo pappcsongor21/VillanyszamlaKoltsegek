03d983dd6007c26e4c1b12b839c1856bebd7a5e0
pappcsongor21 <csoni2002@gmail.com>
2025-03-07 15:28:40 +0100

    YearService class basic properties and ctor

--- BACKEND/VillanyszamlaKoltsegekBackend/VillanyszamlaKoltsegekBackend/YearService.cs
+++ BACKEND/VillanyszamlaKoltsegekBackend/VillanyszamlaKoltsegekBackend/YearService.cs
@@ -1,7 +1,16 @@
-﻿namespace VillanyszamlaKoltsegekBackend
+﻿using VillanyszamlaKoltsegekBackend.Model;
+
+namespace VillanyszamlaKoltsegekBackend
 {
     public class YearService
     {
+        public Year[] Years { get; set; }
+        public int PricePerUnit { get; set; }
 
+        public YearService(Year[] years, int pricePerUnit)
+        {
+            this.Years = years;
+            this.PricePerUnit = pricePerUnit;
+        }
     }
 }
--- BACKEND/VillanyszamlaKoltsegekBackend/VillanyszamlaKoltsegekBackend/obj/Debug/net8.0/VillanyszamlaKoltsegekBackend.AssemblyInfo.cs
+++ BACKEND/VillanyszamlaKoltsegekBackend/VillanyszamlaKoltsegekBackend/obj/Debug/net8.0/VillanyszamlaKoltsegekBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VillanyszamlaKoltsegekBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10631e5c0b431bf14b337892f74a08fcc71025d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adbff8e03411e631b104b1307e5836470628d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("VillanyszamlaKoltsegekBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VillanyszamlaKoltsegekBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
